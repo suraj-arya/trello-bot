d055122e3489fcf5914514ac24e9423a7ad8387f
Suraj Arya <suraj.p.arya@gmail.com>
2017-05-27 15:27:28 +0530

    * temp commit

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+
+# import os
+import json
+from flask import Flask, request
 
 
 app = Flask(__name__)
+# web_hook_secret_key = os.environ.get('WEBHOOK_SECRET_KEY')
 
 
 @app.route("/", methods=['POST'])
 def hello():
+    print(json.loads(request.data))
     return "Hello World!"
 
 
