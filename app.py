2b5efba1184f9604d160c5653ae8a9c6031c2893
Suraj Arya <suraj.p.arya@gmail.com>
2017-05-27 14:09:24 +0530

    Testing

--- app.py
+++ app.py
@@ -3,18 +3,64 @@
 from __future__ import absolute_import
 from __future__ import division
 
-# import os
+import os
 import json
+
 from flask import Flask, request
+
+from .change_list import change_list
 
 
 app = Flask(__name__)
 # web_hook_secret_key = os.environ.get('WEBHOOK_SECRET_KEY')
 
 
-@app.route("/", methods=['POST'])
+@app.route("/", methods=['GET'])
 def hello():
-    print(json.loads(request.data))
+    return 'Hello world!'
+
+
+@app.route("/", methods=['POST'])
+def index():
+    data = json.loads(request.data)
+    header = request.headers.get('X-GitHub-Event', '').strip()
+    print(data)
+
+    if header != 'pull_request':
+        return 'NOOP'
+
+    action = data.get('action')
+    move_to = None
+    if action == 'open':
+        move_to = os.environ.get('UNDER_REVIEW_LIST_ID')
+
+    elif action == 'closed':
+        merged = data['pull_request'].get('merged', False)
+        if merged:
+            move_to = os.environ.get('MERGED_LIST_ID')
+    else:
+        return 'NOOP'
+
+    title = data['pull_request']['title']
+    body = data['pull_request']['body']
+
+    tokens = '{} {}'.format(title, body).split()
+    card_id = None
+    try:
+        for token in tokens:
+            if 'TRELLO' in token.upper():
+                card_id = token.split(':')[-1].strip()
+    except Exception as e:
+        print(e.message)
+        return '', 500
+
+    if None not in (card_id, move_to):
+        try:
+            change_list(card_id, move_to)
+        except Exception as e:
+            print(e.message)
+            return '', 500
+
     return "Hello World!"
 
 
--- change_list.py
+++ change_list.py
@@ -4,19 +4,17 @@ from __future__ import absolute_import
 from __future__ import division
 
 from trello import TrelloClient
+import os
 
 
 client = TrelloClient(
-    api_key='',
-    api_secret='',
-    token='',
-    token_secret='')
-
-list_id_to = ''
-card_id = ''
+    api_key=os.environ.get('TRELLO_API_KEY'),
+    api_secret=os.environ.get('TRELLO_SECRET_KEY'),
+    token=os.environ.get('TRELLO_OAUTH_TOKEN'),
+    token_secret=os.environ.get('TRELLO_TOKEN_SECRET'))
 
 
-def move_card(card_id, list_id_from, list_id_to):
+def move_card(card_id, list_id_to):
     """
     """
     card = client.get_card(card_id)
